e21b09bac01a07c6e2724ff1bc6b1cd920aba671
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-09-09 15:19:53 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,11 +14,11 @@ int d;
 for (d = '0'; d <= '9'; d++)
 {
 	putchar(d);
-	if (d !='9')
+	if (d != '9')
 	{
 		putchar(',';
-putchar(' ');
-}
+                putchar(' ');
+          }
 }
 putchar('\n');
 
