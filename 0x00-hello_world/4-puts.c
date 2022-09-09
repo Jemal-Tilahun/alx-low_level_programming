fb898224e63691823b2c6e8cd416122d00d02dfa
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-09-08 17:12:32 -0700

    Hello world #4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+/**
+ * main - ptint the string in the put function
+ *
+ * Description: using the main function 
+ * ithis program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle"
 );
 	return(0);
 }
-
+^c
