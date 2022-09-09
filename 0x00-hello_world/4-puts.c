f0d77f984326e65573084ca5167bc42bd36c1db2
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-09-08 17:39:58 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return: Always 0 (success)
- */ 
+ * Return: Always  success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle"
