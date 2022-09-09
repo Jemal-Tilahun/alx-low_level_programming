db74eb3311f35d3728f537699232a8425e254952
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-09-09 13:22:06 -0700

    Task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * nsin - Entry point
+ *
+ * Return: Always success
+ */
+int main(void)
+{
+	
+int i;
+
+for (i = 0; i < 10; i++)
+	printf("%d", i);
+printf("\n");
+
+return (0);
+}
