0674b6dc259efc72e9cdf2bb5eb6ada250796ae1
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-09-13 15:39:39 -0700

    Write a function that adds two integers and returns the result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * add - Add to intigers
- * num1 = first, num2 = secod intiger
+ * add - Add two intigers
+ * @num1: first intiger
+ * @num2: secod intiger
  * Return: Always success
  */
 int add(int num1, int num2)
