406664b594a771570c4171c4e334241fecf7d1bf
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-09-13 06:45:53 -0700

    Write a function that prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,18 @@
-_putchar
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_alphabe(void);
+void print_alphabe_×10(void);
+int islower(int c);
+int _putchar(char c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
