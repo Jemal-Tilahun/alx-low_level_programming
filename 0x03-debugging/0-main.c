e81cdd897c9c67d37615b9d0331416c1c049e991
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-09-15 01:23:00 -0700

    check positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - tests function that prints if integer is positive or negative
+ * main - tests function that prints if integer is p or n
  *
  * Return: 0
  */
@@ -8,7 +8,7 @@ int main(void)
 {
 int i;
 
-i = 98;
+i = 0;
 positive_or_negative(i);
 
 return (0);
