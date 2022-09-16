317b881f66de9e4895d0d27c2e6775fc4ced13a9
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-09-16 12:18:03 -0700

    Task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,7 +17,7 @@ char fb[] = "FizzBuzz";
 for (i = 1; i <= 100; i++)
 {
 if (i == 100)
-printf("%s", b
+printf("%s", b);
 else if ((i % 3 == 0) && (i % 5 == 0))
 printf("%s ", fb);
 else if (i % 3 == 0)
