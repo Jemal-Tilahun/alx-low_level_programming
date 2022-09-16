3fd60d43a6c1cf3810c088390224005ef2786de6
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-09-15 19:09:04 -0700

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,17 @@
  */
 void more_numbers(void)
 {
+
 int i, ro;
-for (ro = 0; ro < 10; ro++)
+
+for (ro = 0; ro <= 9; ro++)
 {
 for (i = 0; i <= 14; i++)
 {
-if (i >= 10)
-{
+if (i > 9)
+
 _putchar((i / 10) + '0');
-}
+
 _putchar((i % 10) + '0');
 }
 _putchar('\n');
