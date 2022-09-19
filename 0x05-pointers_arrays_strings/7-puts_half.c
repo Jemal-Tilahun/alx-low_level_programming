9a2d522a22358659480f0d851e494c64dcade52e
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-09-19 12:52:33 -0700

    Task7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,25 +5,23 @@
  *
  * Return: void
  */
-void puts_half(char *str)
+void puts_half(char *str)
 {
-         int j = 0;
-         int k;
-
-         while (str[j] != '\0')
-         {
-                 j++;
-         }
-
-         if (j % 2 == 1)
-         {
-                 k = (j - 1) / 2;
-                 k += 1;
-         }
-         else
-         {
-                 k = j / 2;
-         }
+int j = 0;
+int k;
+while (str[j] != '\0')
+{
+j++;
+}
+if (j % 2 ==1)
+{
+k = (j - 1) / 2;
+k += 1;
+}
+else 
+{
+k = j / 2;
+}
 for (; k < j; k++)
 {
 _putchar(str[k]);
