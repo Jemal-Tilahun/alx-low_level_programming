c6dfab2971d27588fc9255ef4711eaaad4cb9612
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-09-19 11:53:31 -0700

    Task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * puts2 - print every other character of a string 
- * @str: The string to be treated
+ * puts2 - print every other character of a string
+ * @str: The string to be treate
  *
  * Return: void
  */
+
 void puts2(char *str)
 {
 int i;
