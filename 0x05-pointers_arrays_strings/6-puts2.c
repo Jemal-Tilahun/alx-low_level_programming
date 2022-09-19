9ecb97635f5d73d9b29cfabbcb03c42d7ef67104
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-09-19 11:41:13 -0700

    Task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,22 @@
-#include "main.h"
- /** 
-   * puts2 - Prints every other character of a string 
-   * @str: The string to be treated 
-   * 
-   * Return: void 
-   */ 
- void puts2(char *str) 
- { 
-         int i; 
-         int j = 0; 
-  
-         while (str[j] != '\0') 
-         { 
-                 j++; 
-         } 
-  
-         for (i = 0; i < j; i += 2) 
-         { 
-                 _putchar(str[i]); 
-         } 
-  
-         _putchar('\n'); 
- }
+#include "main.h"
+/**
+ * puts2 - print every other character of a string 
+ * @str: The string to be treated
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+int i;
+int j = 0;
+
+while (str[j] != '\0')
+{
+j++;
+}
+for (i = 0; i < j; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
