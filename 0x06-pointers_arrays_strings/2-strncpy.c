637557f91f910a7d6c1995260fbc4c1996f95238
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-09-21 15:38:17 -0700

    Task1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,33 +1,29 @@
-#include "main.h" 
-  
- /** 
-   * _strncpy - Copy a string 
-   * @dest: The destination value 
-   * @src: The source value 
-   * @n: The copy limit 
-   * 
-   * Return: char value 
-   */ 
- char *_strncpy(char *dest, char *src, int n) 
- { 
-         int a = 0, b = 0; 
-  
-         while (src[b]) 
-         { 
-                 b++; 
-         } 
-  
-         while (a < n && src[a]) 
-         { 
-                 dest[a] = src[a]; 
-                 a++; 
-         } 
-  
-         while (a < n) 
-         { 
-                 dest[a] = '\0'; 
-                 a++; 
-         } 
-  
-         return (dest); 
- }
+#inlude "main.h"
+/**
+ * _strncpy - copy string
+ * @dest: destination value
+ * @src: the sorce value
+ * @n: the copy limit
+ *
+ * Return: char value
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int a = 0, b = 0;
+while (src[b])
+{
+b++;
+}
+while (a < n && src[a])
+{
+dest[a] = src[a];
+a++;
+}
+while (a < n)
+{
+dest[a] = '\0';
+a++;
+}
+return (dest);
+}
