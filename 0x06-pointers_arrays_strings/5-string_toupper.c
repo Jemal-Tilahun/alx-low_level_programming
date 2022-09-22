32ead37d4fc3483b523fdead4123f125e4fd35d1
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-09-22 02:05:04 -0700

    Task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,32 +1,22 @@
-#include "main.h" 
-#include <stdio.h>
-  
- /** 
-   * cap_string - ... 
-   * @s: ... 
-   * 
-   * Return: char value 
-   */ 
- char *cap_string(char *s) 
- { 
-         int a = 0, i; 
-         int cspc = 13; 
-         char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'}; 
-  
-         while (s[a]) 
-         { 
-                 i = 0; 
-  
-                 while (i < cspc) 
-                 { 
-                         if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122)) 
-                                 s[a] -= 32; 
-  
-                         i++; 
-                 } 
-  
-                 a++; 
-         } 
-  
-         return (s); 
- }
+#include "main.h"
+/**
+ * string_toupper - change all lower case letters to uppercase
+ * @p: The string will be modified
+ *
+ * Return: char var
+ */
+char *string_toupper(char *p)
+{
+int a = 0;
+
+while (p[a])
+{
+
+if (p[a] >= 97 && p[a] <= 122)
+{
+p[a] -= 32;
+}
+a++;
+}
+return (p);
+}
