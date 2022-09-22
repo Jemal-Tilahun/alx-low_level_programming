8fab4a7824837511ba5470748cf9cc879d59f612
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-09-22 14:01:25 -0700

    Task12

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -30,8 +30,10 @@
                  num = (*n1 - '0') + tens; 
                  *(r + r_index) = (num % 10) + '0'; 
                  tens = num / 10; 
-         } 
-  
+         }
+
+
+
          for (; *n2; n2--, r_index--) 
          { 
                  num = (*n2 - '0') + tens; 
