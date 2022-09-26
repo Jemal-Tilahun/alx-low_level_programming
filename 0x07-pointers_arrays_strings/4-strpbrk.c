d86846b00a4b2ad7d6aaa4143345aa18f799d461
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-09-26 11:43:51 -0700

    Task4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,33 +1,28 @@
-#include "main.h" 
-  
- /** 
-   * _strpbrk - search a string for any of a set of bytes 
-   * @s: source string 
-   * @accept: accepted characters 
-   * 
-   * Return: the string since the first found accepted character 
-   */ 
- char *_strpbrk(char *s, char *accept) 
- { 
-         int a = 0, b; 
-  
-         while (s[a]) 
-         { 
-                 b = 0; 
-  
-                 while (accept[b]) 
-                 { 
-                         if (s[a] == accept[b]) 
-                         { 
-                                 s += a; 
-                                 return (s); 
-                         } 
-  
-                         b++; 
-                 } 
-  
-                 a++; 
-         } 
-  
-         return ('\0'); 
- }
+#include "main.h"
+
+/**
+ * _strpbrk - search a string for any of a set of byte
+ * @s: source string
+ * @accept: accepted character
+ *
+ * Return: the string since the first found accepted character
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int a = 0, b;
+while (s[a])
+{
+b = 0;
+while (accept[b])
+{
+if (s[a] == accept[b])
+{
+s += a;
+return (s);
+}
+b++;
+}
+a++;
+}
+return ('\0');
+
