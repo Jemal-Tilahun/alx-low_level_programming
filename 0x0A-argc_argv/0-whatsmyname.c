2231b52dd5a15fe3987ed0733b897d6174c58827
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-09-30 14:09:07 -0700

    Task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the program name
+ * @argc: The number of arguments
+ * @argv: An array of pointers
+ *
+ * Return: Always 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
