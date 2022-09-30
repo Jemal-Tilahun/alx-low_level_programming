5ad994e82a57448deccbc09742fc6772b123c981
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-09-30 14:20:15 -0700

    Task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the number of arguments
+ * @argc: The number of argument
+ * @argv: An array of pointer
+ *
+ * Return: Always 0
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+printf("%d\n", argc - 1);
+
+return (0);
+}
