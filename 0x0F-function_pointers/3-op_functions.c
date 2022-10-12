9b498a75f58e94d53ee9232f6b233ca728b2eabf
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-10-12 12:18:55 -0700

    Task3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,75 +1,70 @@
-#include "3-calc.h" 
- #include <stdlib.h> 
- #include <stdio.h> 
-  
- /** 
-   * op_add - ... 
-   * @a: ... 
-   * @b: ... 
-   * 
-   * Return: ... 
-   */ 
- int op_add(int a, int b) 
- { 
-         return (a + b); 
- } 
-  
- /** 
-   * op_sub - ... 
-   * @a: ... 
-   * @b: ... 
-   * 
-   * Return: ... 
-   */ 
- int op_sub(int a, int b) 
- { 
-         return (a - b); 
- } 
-  
- /** 
-   * op_mul - ... 
-   * @a: ... 
-   * @b: ... 
-   * 
-   * Return: ... 
-   */ 
- int op_mul(int a, int b) 
- { 
-         return (a * b); 
- } 
-  
- /** 
-   * op_div - ... 
-   * @a: ... 
-   * @b: ... 
-   * 
-   * Return: ... 
-   */ 
- int op_div(int a, int b) 
- { 
-         if (b == 0) 
-         { 
-                 printf("Error\n"); 
-                 exit(100); 
-         } 
-  
-         return (a / b); 
- } 
-  
- /** 
-   * op_mod - ... 
-   * @a: ... 
-   * @b: ... 
-   * 
-   * Return: ... 
-   */ 
- int op_mod(int a, int b) 
- { 
-         if (b == 0) 
-         { 
-                 printf("Error\n"); 
-                 exit(100); 
-         } 
-  
-         return (a % b); 
- }
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * op_add - ___
+ * @a: ___
+ * @b: ___
+ *
+ * Return: ___
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - ___
+ * @a: ___
+ * @b: ___
+ *
+ * Return: ___
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - ___
+ * @a: ___
+ * @b: ___
+ *
+ * Return: ___
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - ___
+ * @a: ___
+ * @b: ___
+ *
+ * Return: ___
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+ * op_mod- ___
+ * @a: ___
+ * @b: ___
+ *
+ * Return: ___
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
+
