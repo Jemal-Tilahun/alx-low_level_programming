672115bcb0ac627c46523ed4820a35b2cdaf6fe2
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-10-12 10:03:07 -0700

    Task1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,23 +1,22 @@
-#include "function_pointers.h" 
-  
- /** 
-   * array_iterator - ... 
-   * @array: ... 
-   * @size: ... 
-   * @action: ... 
-   * 
-   * Return: ... 
-   */ 
- void array_iterator(int *array, size_t size, void (*action)(int)) 
- { 
-         unsigned int i = 0; 
-  
-         if (array != NULL && action != NULL && size > 0) 
-         { 
-                 while (i < size) 
-                 { 
-                         action(array[i]); 
-                         i++; 
-                 } 
-         } 
- }
+#include "function_pointers.h"
+/**
+ * array_iterator - ___
+ * @array: ___
+ * @size: ___
+ * @action: ___
+ *
+ * Return: ___
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (array != NULL && action != NULL && size > 0)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+}
