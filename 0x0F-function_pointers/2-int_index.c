22e0f68232b5493bdf53a6dc542c705cd59206a8
Jemal-Tilahun <jemaltilahun14@gmail.com>
2022-10-12 10:30:50 -0700

    Task2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,30 +1,28 @@
-#include "function_pointers.h" 
-  
- /** 
-   * int_index - ... 
-   * @array: ... 
-   * @size: ... 
-   * @cmp: ... 
-   * 
-   * Return: ... 
-   */ 
- int int_index(int *array, int size, int (*cmp)(int)) 
- { 
-         int i = 0; 
-  
-         if (size > 0) 
-         { 
-                 if (array != NULL && cmp != NULL) 
-                 { 
-                         while (i < size) 
-                         { 
-                                 if (cmp(array[i])) 
-                                         return (i); 
-  
-                                 i++; 
-                         } 
-                 } 
-         } 
-  
-         return (-1); 
- }
+#include "function_pointers.h"
+/**
+ * int_index - ___
+ * @array: ___
+ * @size: ___
+ * @cmp: ___
+ *
+ * Return: ___
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+	{
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+			i++;
+		}
+	}
+}
+return (-1);
+}
+
